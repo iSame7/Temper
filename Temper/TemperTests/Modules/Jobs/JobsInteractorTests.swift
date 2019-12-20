bf2649194addb8ed4af67fd0d51fd2f439e9e245
Sameh Mabrouk <mabrouksameh@gmail.com>
2019-12-20 22:09:42 +0100

    Split JobsServiceMock in a new file

--- Temper/TemperTests/Modules/Jobs/JobsInteractorTests.swift
+++ Temper/TemperTests/Modules/Jobs/JobsInteractorTests.swift
@@ -63,20 +63,3 @@ class JobsInteractorTests: XCTestCase {
         }
     }
 }
-
-class JobsServiceMock: JobsFetching {
-    var invokedFetchJobsFor = false
-    var invokedFetchJobsForCount = 0
-    var invokedFetchJobsForParameters: (dates: String, Void)?
-    var invokedFetchJobsForParametersList = [(dates: String, Void)]()
-    var stubbedFetchJobsForCompletionResult: ([String: [Job]]?, TemperError?)?
-    func fetchJobsFor(dates: String, completion: @escaping ([String: [Job]]?, TemperError?) -> Void) {
-        invokedFetchJobsFor = true
-        invokedFetchJobsForCount += 1
-        invokedFetchJobsForParameters = (dates, ())
-        invokedFetchJobsForParametersList.append((dates, ()))
-        if let result = stubbedFetchJobsForCompletionResult {
-            completion(result.0, result.1)
-        }
-    }
-}
